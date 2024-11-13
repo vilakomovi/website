a4b5b8afd677471d48861ddc4a421dcda56d3d4b
vilakomovi <vila.komovi.zlatibor@gmail.com>
2024-11-14 00:27:06 +0100

    Add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website',
-  build: {
-    chunkSizeWarningLimit: 1000, // Adjust this number as needed (in KB)
-  },
+  // base: '/website',
+  // build: {
+  //   chunkSizeWarningLimit: 1000, // Adjust this number as needed (in KB)
+  // },
 })
