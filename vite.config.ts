ce628ef285d56c8fe74fb4a207f5b3d38d00ce65
vilakomovi <vila.komovi.zlatibor@gmail.com>
2024-11-13 16:36:31 +0100

    Add chunk size options

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website'
+  base: '/website',
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust this number as needed (in KB)
+  },
 })
