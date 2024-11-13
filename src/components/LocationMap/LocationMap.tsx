1fb72fe3cc250ad9e28891ea2c86cddf613252cc
vilakomovi <vila.komovi.zlatibor@gmail.com>
2024-11-13 16:15:03 +0100

    Add robots.txt in public, updating vite config

--- src/components/LocationMap/LocationMap.tsx
+++ src/components/LocationMap/LocationMap.tsx
@@ -2,12 +2,9 @@ import {APIProvider, Map, Marker} from "@vis.gl/react-google-maps";
 
 const LocationMap = () => {
     const placeLocation = {
-        // lat: 43.737514876317356,
-        // lng: 19.712283566103256,
         lat: 43.736823,
         lng: 19.711369,
     }
-
 
     const styles = {
         borderRadius: '20px',
