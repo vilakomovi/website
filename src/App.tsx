8ba6c8806a321b2460b41f26fba3b6d571fb6deb
vilakomovi <vila.komovi.zlatibor@gmail.com>
2024-11-14 15:06:19 +0100

    Add new image for sanitarije

--- src/App.tsx
+++ src/App.tsx
@@ -227,7 +227,7 @@ function App() {
                     <Flex gap="2rem" justifyContent="center" flexDirection={{ base: 'column', md: 'row' }}>
                         <Card maxW="md">
                             <CardHeader>
-                                <Image src="/images/sanitarije.jpg" objectFit="cover" h="200px" w="100%" />
+                                <Image src="/images/kupatilo.jpg" objectFit="cover" h="200px" w="100%" />
                             </CardHeader>
                             <CardBody>
                                 <VStack w="full" h="full" spacing={10} alignItems="flex-start">
